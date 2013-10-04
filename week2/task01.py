4e687c309701780fb5723ebaa1ea615136f4ad00
Ingrid Avendaño <ingridavendano@gmail.com>
2013-10-04 16:03:35 -0700

    Reverse list function.

---
+++ week2/task01.py
@@ -0,0 +1,21 @@
+#!/bin/env python
+
+"""
+Given list l, sort it in reverse order
+ie: 10, 9, 8, 7, 6
+"""
+
+def reverse(l):
+	for i in range(len(l)/2):
+		tmp = l[i]
+		l[i] = l[-i-1]
+		l[-i-1] = tmp
+
+	return l
+
+
+l = [5,2,1,5,9,10,11]
+
+print "Before:",l
+reverse(l)
+print "After:",l
